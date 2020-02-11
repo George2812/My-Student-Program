25daf5088765d79901b3801d88fde5703ea661d9
George Dialynas <dialynas1@yahoo.com>
2020-02-11 15:03:02 +0200

    Building Read Class

--- Main.java
+++ Main.java
@@ -1,26 +1,13 @@
+import java.util.ArrayList;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		Student S1;
-		S1=new Student("George","1324");
 		
-		Course C1=new Course("Java",2);
-		Course C2=new Course("C#",3);
+		ArrayList<Student> students=new ArrayList<Student>();
 		
-		S1.addCourse(C1);
-		S1.addCourse(C2);
-		
-		S1.printInfo();
-		
-		GraduateStudent GS1=new GraduateStudent("John","8976","Kalianidis");
-		
-		GS1.addCourse(C1);
-		GS1.addCourse(C2);
-		
-		GS1.printInfo();
-
+	
 	}
 
 }
--- Read.java
+++ Read.java
@@ -1,4 +1,51 @@
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+
+
 
 public class Read {
+
+	public void readData(ArrayList<Student> students) {
+		
+		boolean more=true;
+		
+		while(more) {
+			
+			String selection=JOptionPane.showInputDialog("1: Student,2: Graduate");
+			
+			int choice=Integer.parseInt(selection);
+			
+			String name=JOptionPane.showInputDialog("Name");
+			
+			String id=JOptionPane.showInputDialog("Id");
+			
+			String supervisor=null;
+			
+			if(choice==2) {
+				
+				supervisor=JOptionPane.showInputDialog("Supervisor");
+			}
+			
+			Student student;
+			
+			if(choice==1) {
+				
+				student=new Student(name,id);
+			}
+			else {
+				
+				student=new GraduateStudent(name,id,supervisor);
+			}
+			
+			String answer=JOptionPane.showInputDialog("More Students? (Y/N)");
+			if(answer.equals("N"))
+				more=false;
+			
+			
+		}
+		
+		
+	}
 
 }
